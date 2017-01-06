67762088a8fd3eb60e7745548de94f8e3abdf71d
xpin <xpin_1509@163.com>
2017-01-06 11:26:00 +0800

    delete more

--- test2.js
+++
@@ -1,8 +0,0 @@
-<%}else if(2 < curentPage < totalPage-2){%>
-								<li><a>...</a></li>
-								<li><a href="/?page=<%= curentPage-1%>"><%= curentPage-1%></a></li>
-								<li><a href="/?page=<%= curentPage%>"><%= curentPage%></a></li>
-								<li class="active"><a href="/?page=<%= curentPage+1%>"><%= curentPage+1%></a></li>
-								<li><a href="/?page=<%= curentPage+2%>"><%= curentPage+2%></a></li>
-								<li><a href="/?page=<%= curentPage+3%>"><%= curentPage+3%></a></li>
-								<li><a>...</a></li>
